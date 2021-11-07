d6927133ffc1c4973de03e9c0f4ebf48afc51515
davidju4717 <jud@oregonstate.edu>
2021-11-07 16:27:45 -0600

    Integration of microservices

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, json, redirect, jsonify, url_for, flash
 from flask import request
+import requests
 
 app = Flask(__name__)
 
@@ -12,5 +13,15 @@ def index():
 def dish():
     if request.method == 'POST':
         dish = request.form.get("group1")
-        dish_description = "Bibimbap[1] (/ˈbiːbɪmbæp/ BEE-bim-bap,[2] from Korean 비빔밥 [pi.bim.p͈ap̚], literally 'mixed rice'), sometimes romanized as bi bim bap or bi bim bop, is a Korean rice dish. The term 'bibim' means mixing rice (burned rice at the bottom of the dish and cooked rice), while the 'bap' noun refers to rice. Bibimbap is served as a bowl of warm white rice topped with namul (sautéed and seasoned vegetables) or kimchi (traditional fermented vegetables) and gochujang (chili pepper paste), soy sauce, or doenjang (a fermented soybean paste). A raw or fried egg and sliced meat (usually beef) are common additions. The hot dish is stirred together thoroughly just before eating.[3] In South Korea, Jeonju, Jinju, and Tongyeong are especially famous for their versions of bibimbap.[4] In 2011, the dish was listed at number 40 on the World's 50 most delicious foods readers' poll compiled by CNN Travel."
-        return render_template("dish.html", dish=dish, dish_description=dish_description)
+
+        # obtain dish summary with wikipedia scraper
+        url = "https://wiki-scraper-331405.uc.r.appspot.com/"+dish+"/summary"
+        dish_description = requests.get(url).json()["summary"]
+
+        # obtain dish image url with google image scraper
+        # data to be sent to api
+        url = "https://mycovidupdatermicroservice.herokuapp.com/imgScraper"
+        data = {"imgSize": "LARGE", "fileType": "jpeg", "imgType": "photo", "num": 1, "q": dish}
+        dish_image = requests.post(url, json=data).json()["0"]
+
+        return render_template("dish.html", dish=dish, dish_image=dish_image, dish_description=dish_description)
